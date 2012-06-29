117f39cce6fa87737a61ad9944cba35342894e39
Adam Burley <adam@hawknetwork.org>
2012-06-29 17:58:28 +0100

    Attempt to merge projects; separation of common parent and GUI

---
+++ hawkrota-gui/src/main/java/org/hawknetwork/hawkrota/gui/test/HawkRotaMain.java
@@ -0,0 +1,12 @@
+package org.hawknetwork.hawkrota.gui.test;
+
+public class HawkRotaMain {
+
+	/**
+	 * @param args The arguments passed on the command line
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ hawkrota-gui/src/test/java/org/hawknetwork/hawkrota/gui/test/HawkRotaMainTest.java
@@ -0,0 +1,5 @@
+package org.hawknetwork.hawkrota.gui.test;
+
+public class HawkRotaMainTest {
+
+}
